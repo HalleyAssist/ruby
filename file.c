682d3897a530fc4304d94181803df7ddc46f2f10
Mathew Heard <splitice@users.noreply.github.com>
2017-12-20 07:34:25 +1100

    fix expansion

--- file.c
+++ file.c
@@ -5787,10 +5787,11 @@ rb_find_file_ext_safe(VALUE *filep, const char *const *ext, int safe_level)
 	expanded = 1;
     }
 
-    if (expanded && rb_is_absolute_path(f)) {
+    if (rb_is_absolute_path(f)) {
 		if (safe_level >= 1 && !fpath_check(fname)) {
 			rb_raise(rb_eSecurityError, "loading from unsafe path %s", f);
 		}
+		if (!expanded) fname = file_expand_path_1(fname);
 		if (rb_file_load_ok(RSTRING_PTR(fname)) == 0) {
 			return 0;
 		}
